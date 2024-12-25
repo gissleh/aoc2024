faf7dc1c30fc85224cdc1f63233af2a7201aa7d9
Gisle Aune <dev@gisle.me>
2024-12-25 14:46:54 +0100

    add day 24 part 2 (finally)

--- src/2024/day24.rs
+++ src/2024/day24.rs
@@ -1,109 +1,184 @@
+use arrayvec::ArrayVec;
 use common::parser;
 use common::parser::Parser;
 use common::runner::Runner;
 use rustc_hash::FxHashMap;
 
 pub fn main(r: &mut Runner, input: &[u8]) {
-    let wires = r.prep("Parse", || parse(input));
+    let (x, y, wires) = r.prep("Parse", || parse(input));
 
-    r.part("Part 1", || part_1(&wires));
-    r.info("Named gates", &wires.named.len());
+    r.part("Part 1", || part_1(&wires, x, y));
+    r.part("Part 2", || part_2(&wires));
+    r.info("Gates", &wires.map.len());
 }
 
-fn part_1(wires: &Wires) -> u64 {
-    wires.run_z()
+fn part_1(wires: &Wires, x: u64, y: u64) -> u64 {
+    wires.run_z(x, y)
 }
 
-fn parse(input: &[u8]) -> Wires {
-    Wires::parser().parse_value(input).unwrap()
+fn part_2(wires: &Wires) -> String {
+    const Z00: u16 = z_id(0);
+    const Z01: u16 = z_id(1);
+
+    let mut baddies = ArrayVec::<u16, 80>::new();
+
+    let last_z = (0..64)
+        .map(|i| z_id(i))
+        .take_while(|id| wires.map.contains_key(&id))
+        .last()
+        .unwrap();
+
+    for (out, op) in wires.map.iter().map(|(a, b)| (*a, *b)) {
+        match op {
+            Op::XOR(a, b) => {
+                if a.named().is_some() != (Z00 != out && is_z(out)) {
+                    baddies.push(out);
+                    continue;
+                }
+
+                if out != Z00 && out != Z01 {
+                    if let Some((an, a)) = a.named().map(|n| (n, wires.map[&n])) {
+                        if let Some((bn, b)) = b.named().map(|n| (n, wires.map[&n])) {
+                            if a.is_and() {
+                                baddies.push(an);
+                            }
+                            if b.is_and() {
+                                baddies.push(bn);
+                            }
+                        }
+                    }
+                }
+            }
+            Op::OR(a, b) => {
+                if is_z(out) && out != last_z {
+                    baddies.push(out);
+                    continue;
+                }
+
+                if let Some((an, a)) = a.named().map(|n| (n, wires.map[&n])) {
+                    if let Some((bn, b)) = b.named().map(|n| (n, wires.map[&n])) {
+                        if !a.is_and() {
+                            baddies.push(an);
+                        }
+                        if !b.is_and() {
+                            baddies.push(bn);
+                        }
+                    }
+                }
+            }
+            Op::AND(_, _) => {
+                if is_z(out) {
+                    baddies.push(out);
+                    continue;
+                }
+            }
+        }
+    }
+
+    baddies.sort_unstable();
+    let mut res = String::with_capacity(4 * 8);
+    for i in 0..baddies.len() {
+        if i > 0 && baddies[i] == baddies[i - 1] {
+            continue;
+        }
+
+        if !res.is_empty() {
+            res.push(',')
+        }
+
+        for ch in id_to_name(baddies[i]) {
+            res.push(ch as char);
+        }
+    }
+
+    res
+}
+
+fn parse(input: &[u8]) -> (u64, u64, Wires) {
+    xy_parser()
+        .and_discard(b'\n')
+        .and(Wires::parser())
+        .map(|((x, y), wires)| (x, y, wires))
+        .parse_value(input)
+        .unwrap()
+}
+
+fn xy_parser<'i>() -> impl Parser<'i, (u64, u64)> {
+    b'x'.or(b'y')
+        .and(parser::uint::<u32>())
+        .and_discard(b": ")
+        .and(b'0'.or(b'1'))
+        .and_discard(b'\n')
+        .repeat_fold(
+            || (0u64, 0u64),
+            |(x, y), ((xy, i), v)| {
+                if v == b'0' {
+                    (x, y)
+                } else if xy == b'x' {
+                    (x | 1 << i, y)
+                } else {
+                    (x, y | 1 << i)
+                }
+            },
+        )
 }
 
 struct Wires {
-    x: u64,
-    y: u64,
-    named: FxHashMap<u16, Op>,
+    map: FxHashMap<u16, Op>,
 }
 
 impl Wires {
-    fn run_z(&self) -> u64 {
+    fn run_z(&self, x: u64, y: u64) -> u64 {
         let mut res = 0;
         for z in 0..64 {
-            let id = (36 * 36 * 35) + ((z / 10) * 36) + (z % 10);
-
-            if let Some(op) = self.named.get(&id) {
-                if self.run_op(*op) {
+            if let Some(op) = self.map.get(&z_id(z)) {
+                if self.run_op(*op, x, y) {
                     res |= 1 << z;
                 }
+            } else {
+                break;
             }
         }
 
         res
     }
 
-    fn run_ref(&self, r: Ref) -> bool {
+    fn run_ref(&self, r: Ref, x: u64, y: u64) -> bool {
         match r {
-            Ref::X(i) => self.x & 1 << i != 0,
-            Ref::Y(i) => self.y & 1 << i != 0,
-            Ref::Named(n) => self.run_op(self.named[&n]),
-            _ => unreachable!(),
+            Ref::X(i) => x & 1 << i != 0,
+            Ref::Y(i) => y & 1 << i != 0,
+            Ref::Named(n) => self.run_op(self.map[&n], x, y),
         }
     }
 
-    fn run_op(&self, op: Op) -> bool {
+    fn run_op(&self, op: Op, x: u64, y: u64) -> bool {
         match op {
-            Op::AND(a, b) => self.run_ref(a) && self.run_ref(b),
-            Op::OR(a, b) => self.run_ref(a) || self.run_ref(b),
-            Op::XOR(a, b) => self.run_ref(a) ^ self.run_ref(b),
-            _ => unreachable!(),
+            Op::AND(a, b) => self.run_ref(a, x, y) && self.run_ref(b, x, y),
+            Op::OR(a, b) => self.run_ref(a, x, y) || self.run_ref(b, x, y),
+            Op::XOR(a, b) => self.run_ref(a, x, y) ^ self.run_ref(b, x, y),
         }
-    }
-
-    fn initial_parser<'i>() -> impl Parser<'i, (u64, u64)> {
-        b'x'.or(b'y')
-            .and(parser::uint::<u32>())
-            .and_discard(b": ")
-            .and(b'0'.or(b'1'))
-            .and_discard(b'\n')
-            .repeat_fold(
-                || (0u64, 0u64),
-                |(x, y), ((xy, i), v)| {
-                    if v == b'0' {
-                        (x, y)
-                    } else if xy == b'x' {
-                        (x | 1 << i, y)
-                    } else {
-                        (x, y | 1 << i)
-                    }
-                },
-            )
     }
 
     fn parser<'i>() -> impl Parser<'i, Wires> {
-        Self::initial_parser()
-            .and_discard(b"\n")
-            .and(
-                Op::parser()
-                    .and_discard(b" -> ")
-                    .and(Ref::parser())
-                    .delimited_by(b'\n')
-                    .repeat_fold_mut(
-                        || FxHashMap::with_capacity_and_hasher(128, Default::default()),
-                        |named, (op, out)| match out {
-                            Ref::Named(id) => {
-                                named.insert(id, op);
-                            }
-                            _ => panic!("{out:?} is not a valid output"),
-                        },
-                    ),
+        Op::parser()
+            .and_discard(b" -> ")
+            .and(Ref::parser())
+            .delimited_by(b'\n')
+            .repeat_fold_mut(
+                || FxHashMap::with_capacity_and_hasher(128, Default::default()),
+                |named, (op, out)| match out {
+                    Ref::Named(id) => {
+                        named.insert(id, op);
+                    }
+                    _ => panic!("{out:?} is not a valid output"),
+                },
             )
-            .map(|((x, y), named)| Self { x, y, named })
+            .map(|named| Self { map: named })
     }
 }
 
-#[derive(Debug, Clone, Copy, Default, Eq, PartialEq)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
 enum Ref {
-    #[default]
-    Null,
     X(u16),
     Y(u16),
     Named(u16),
@@ -129,12 +204,18 @@ impl Ref {
             }
         })
     }
+
+    fn named(&self) -> Option<u16> {
+        match self {
+            Ref::X(_) => None,
+            Ref::Y(_) => None,
+            Ref::Named(n) => Some(*n),
+        }
+    }
 }
 
-#[derive(Debug, Clone, Copy, Default, Eq, PartialEq)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
 enum Op {
-    #[default]
-    Null,
     XOR(Ref, Ref),
     AND(Ref, Ref),
     OR(Ref, Ref),
@@ -154,6 +235,13 @@ impl Op {
                 _ => unreachable!(),
             })
     }
+
+    fn is_and(&self) -> bool {
+        match self {
+            Self::AND(_, _) => true,
+            _ => false,
+        }
+    }
 }
 
 fn digit(v: u8) -> u8 {
@@ -161,6 +249,32 @@ fn digit(v: u8) -> u8 {
         b'0'..=b'9' => v - b'0',
         b'a'..=b'z' => v - b'a' + 10,
         _ => unreachable!(),
+    }
+}
+
+const fn z_id(z: u16) -> u16 {
+    (36 * 36 * 35) + ((z / 10) * 36) + (z % 10)
+}
+
+fn is_z(id: u16) -> bool {
+    id >= (36 * 36 * 35)
+}
+
+#[inline]
+fn id_to_name(id: u16) -> [u8; 3] {
+    [
+        id_char(id / (36 * 36)),
+        id_char((id / 36) % 36),
+        id_char(id % 36),
+    ]
+}
+
+#[inline]
+fn id_char(id: u16) -> u8 {
+    if id < 10 {
+        (id as u8) + b'0'
+    } else {
+        (id as u8 - 10) + b'a'
     }
 }
 
@@ -231,11 +345,13 @@ tnw OR pbm -> gnj
 
     #[test]
     fn part_1_works_on_simple_example() {
-        assert_eq!(part_1(&parse(EXAMPLE_1)), 4)
+        let (x, y, wires) = parse(EXAMPLE_1);
+        assert_eq!(part_1(&wires, x, y), 4);
     }
 
     #[test]
     fn part_1_works_on_complex_example() {
-        assert_eq!(part_1(&parse(EXAMPLE_2)), 2024)
+        let (x, y, wires) = parse(EXAMPLE_2);
+        assert_eq!(part_1(&wires, x, y), 2024);
     }
 }
